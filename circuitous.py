a5be903b8c87aaa791cc8f227ef9d91d8c91993a
Joe Chen <chenj@cisco.com>
2016-03-15 17:27:04 -0400

    Added stuff for DoD (government)

--- circuitous.py
+++ circuitous.py
@@ -12,27 +12,44 @@ from collections import namedtuple
 
 Version = namedtuple('Version', 'major minor')
 
-class Circle:
+class Circle(object):
     '''
     An advanced circle analytics toolkit
     '''
 
-    version = Version(0, 4)
+    version = Version(0, 8)
     
     def __init__(self, radius):
         'Most people do not read docstrings on magic methods'
         self.radius = radius
+
+    @classmethod
+    def from_bbd(cls, diagonal):
+        'Build a Circle from a bounding box diagonal'
+        radius = diagonal / math.sqrt(2)
+        return cls(radius)
+
+    @property
+    def radius(self):
+        return self.diameter / 2.0
+
+    @radius.setter
+    def radius(self, value):
+        self.diameter = value * 2.0
         
     def __repr__(self):
         return ('%s(radius=%r)' % (self.__class__.__name__, self.radius))
 
     def area(self):
         'Perform quadrature on a planar of uniform revolution'
-        return math.pi * self.radius ** 2
+        radius = self.__perimeter() / math.pi / 2.0
+        return math.pi * radius ** 2
 
     def perimeter(self):
         'Something really smart sounding...'
         return math.pi * self.radius * 2
+
+    __perimeter = perimeter
 
     'turn off self'
     @staticmethod
@@ -41,8 +58,8 @@ class Circle:
         Returns a percent grade given an
         inclinometer reading in degrees
 
-            >>> Circle.angle_to_grade(7)
-            12.27845609029046
+            >>> print Circle.angle_to_grade(7)
+            12.2784560903
         '''
         return math.tan(math.radians(angle)) * 100
 
