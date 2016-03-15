549178e1b27e58f33c66103bd3d30022f0c45820
Joe Chen <chenj@cisco.com>
2016-03-15 10:51:33 -0400

    add nice docstrings

--- circuitous.py
+++ circuitous.py
@@ -18,18 +18,33 @@ class Circle:
     '''
 
     version = Version(0, 4)
-
+    
     def __init__(self, radius):
+        'Most people do not read docstrings on magic methods'
         self.radius = radius
         
     def __repr__(self):
         return ('%s(radius=%r)' % (self.__class__.__name__, self.radius))
 
     def area(self):
+        'Perform quadrature on a planar of uniform revolution'
         return math.pi * self.radius ** 2
 
     def perimeter(self):
+        'Something really smart sounding...'
         return math.pi * self.radius * 2
+
+    'turn off self'
+    @staticmethod
+    def angle_to_grade(angle):
+        '''
+        Returns a percent grade given an
+        inclinometer reading in degrees
+
+            >>> Circle.angle_to_grade(7)
+            12.27845609029046
+        '''
+        return math.tan(math.radians(angle)) * 100
 
 if __name__ == "__main__":
     c = Circle(5)
